0c781b4f5e5ca7b9b2410004e3bb309c37900d77
praveesh91 <praveesh91@gmail.com>
2025-08-12 00:09:01 +0530

    not found based on route params

--- src/app/products/[productsId]/reviews/[reviewsId]/not-found.tsx
+++ src/app/products/[productsId]/reviews/[reviewsId]/not-found.tsx
@@ -1,9 +1,17 @@
+"use client";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const NotFound = () => {
+  const pathname = usePathname();
+  const productId = pathname.split("/")[2];
+  const reviewId = pathname.split("/")[4];
+
   return (
     <div>
-      <h2>Review not found</h2>
+      <h2>
+        Review {reviewId} not found for product {productId}
+      </h2>
     </div>
   );
 };
